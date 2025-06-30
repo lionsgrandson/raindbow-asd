9952b1a7d78a613176f954701d880d1702b15f0c
Moshe Schwartzberg <43815731+lionsgrandson@users.noreply.github.com>
2025-07-01 00:03:34 +0300

    feat: put in the hero section content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Icon from "./img/Logo_icon_noshadow.png";
+import sivanHeadShot from "./img/sivanHeadShot.png.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <img src={Icon} alt="a puzzle peice with a rainbow colors"></img>
       </header>
+      <section>
+        <h1>
+          סיון ורונסקי - ליווי מתבגרים ובוגרים על הרצף האוטיסטי והדרכת הורים
+        </h1>
+        <div>
+          <h2>האם פעם שאלתם את עצמכם...</h2>
+          <ul>
+            <li>איך ללמד את הילד שלי לנסוע לבד באוטובוס?</li>
+            <li>האם מתאים לו שירות צבאי, לאומי או בכלל מסגרת אחרת?</li>
+            <li>איך בוחרים מסגרת לימודים או עבודה לבוגרים עם אוטיזם?</li>
+            <li>איך שומרים על שגרת יום יציבה בגיל ההתבגרות?</li>
+          </ul>
+          <img
+            src={sivanHeadShot}
+            alt="headshot of Sivan the owner of the business"
+          />
+          <p>
+            אני כאן בשבילכם – ללוות, להדריך ולבנות יחד מסלול מותאם אישית לילד
+            שלכם, צעד אחר צעד. <br />
+            <br />
+            ליווי אישי למתבגרים ובוגרים עם אוטיזם, סדנאות להורים, וייעוץ מקצועי
+            מבוסס ניסיון אישי, קליני ואקדמי.
+          </p>
+          <h2>מלאו פרטים בטופס ואחזור אליכם לשיחת היכרות</h2>
+        </div>
+      </section>
     </div>
   );
 }
