966cf28460f8c6ac40772ce1b836a647f0d97ca2
Moshe Schwartzberg <43815731+lionsgrandson@users.noreply.github.com>
2025-07-01 09:22:49 +0300

    feat: added the form and text field

--- src/component/contactSec.jsx
+++ src/component/contactSec.jsx
@@ -1,16 +1,25 @@
 import TextField from "@mui/material/TextField";
 import { MdEmail } from "react-icons/md";
 import { FaPhoneAlt } from "react-icons/fa";
-
 function ContactSec() {
   return (
     <section>
       <h2>יצירת קשר</h2>
       <div>
-        <form>
-          <TextField type="text" label="שם" />
+        <form method="POST" autocomplete="on">
+          <TextField type="text" label="שם" autocomplete="on" />
           <TextField type="phone" label="טלפון" />
+          <TextField type="email" label="מייל" />
           <TextField type="text" label="הודעה" />
+          <TextField
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              console.log("btn clicked");
+            }}
+          >
+            Send
+          </TextField>
         </form>
         <div>
           <a href="tel:+972546495902">054-649-5902</a>
