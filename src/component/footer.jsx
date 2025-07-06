53147f739b85663e66022b7c0c604cc2938cc428
Moshe Schwartzberg <43815731+lionsgrandson@users.noreply.github.com>
2025-07-07 00:11:55 +0300

    style: footer styled

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -4,18 +4,21 @@ import logoLongTxt from "../img/Logo_with_NO_shadow_text.png";
 import "./footer.css";
 export default function Footer() {
   return (
-    <div className="footerSec FirstBox">
+    <div className="footerSec ">
       <img
         src={logoLongTxt}
         alt="on the left the slogan on the right a puzzle piece with rainbow colors"
       />
-      <div>
-        <FaPhoneAlt />
-        <a href="tel:+972546495902">054-649-5902</a>
-      </div>
-      <div>
-        <MdEmail />
-        <a href="mailto:LEVANONSKI@GMAIL.COM">levanonski@gmail.com</a>
+      <div className="ContactFooter">
+        <div>
+          <a href="tel:+972546495902">054-649-5902</a>
+          {"  "} <FaPhoneAlt />
+        </div>
+        <div>
+          <a href="mailto:LEVANONSKI@GMAIL.COM">levanonski@gmail.com</a>
+          {"  "}
+          <MdEmail />
+        </div>
       </div>
     </div>
   );
