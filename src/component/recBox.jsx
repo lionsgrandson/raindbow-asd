3c5e4c93eaf35b9c9eb9129c19c10b0988a1488e
Moshe Schwartzberg <43815731+lionsgrandson@users.noreply.github.com>
2025-08-20 00:43:39 +0300

    style: changed stars on the recomendation to the logo

--- src/component/recBox.jsx
+++ src/component/recBox.jsx
@@ -1,9 +1,36 @@
 import "../App.css";
-
+import "./recBox.css";
+import logo from "../img/Logo_icon_noshadow.png";
 function RecBox(props) {
   return (
     <div className="FirstBox divrecbox">
-      <p className="StartRev">★★★★★</p>
+      <p className="StartRev">
+        <img
+          src={logo}
+          className="startLogo"
+          alt="puzzle peice with  rainbow colors"
+        />
+        <img
+          src={logo}
+          className="startLogo"
+          alt="puzzle peice with  rainbow colors"
+        />
+        <img
+          src={logo}
+          className="startLogo"
+          alt="puzzle peice with  rainbow colors"
+        />
+        <img
+          src={logo}
+          className="startLogo"
+          alt="puzzle peice with  rainbow colors"
+        />
+        <img
+          src={logo}
+          className="startLogo"
+          alt="puzzle peice with  rainbow colors"
+        />
+      </p>
       <p className="RevPCon">{props.RevText}</p>
       <p className="RevName">{props.RevName}</p>
     </div>
