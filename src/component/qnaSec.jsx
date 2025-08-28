5373aaa34d328faa8ea50884ec92292878dd4a26
Moshe Schwartzberg <43815731+lionsgrandson@users.noreply.github.com>
2025-08-28 10:14:28 +0300

    edit: removed double spacing

--- src/component/qnaSec.jsx
+++ src/component/qnaSec.jsx
@@ -1,47 +1,47 @@
-import React, { useEffect, useState } from "react";
-import "./qna.css";
-import logo from "../img/Logo_icon_noshadow.png";
+import React, { useEffect, useState } from 'react'
+import './qna.css'
+import logo from '../img/Logo_icon_noshadow.png'
 
 const items = [
   {
-    title: "מהן התוכניות שאני מציעה?",
-    content: `ליווי אישי והדרכה של משלבות בחינוך הרגיל (שילוב יחידני).<br />הדרכת הורים מותאמת להורים שבוחרים לשלב את ילדם.<br />הדרכת צוותים חינוכיים לקיום שילוב יחידני מיטבי ואיכותי.<br />הנחיית בוגרים ומתבגרים בתחומי שינוי ומעבר (צבא, שירות לאומי, תעסוקה, דיור ומיומנויות בין אישיות).<br /><br />בניית הרצאות מותאמות עבור גופים פרטים וציבוריים בנושא אוטיזם – שילוב והכלה.<br /><br />הדרכת מעסיקים כיצד ניתן לשלב בתחומי עבודה שונים אדם על הרצף האוטיסטי.`,
+    title: 'מהן התוכניות שאני מציעה?',
+    content: `ליווי אישי והדרכה של משלבות בחינוך הרגיל (שילוב יחידני).<br />הדרכת הורים מותאמת להורים שבוחרים לשלב את ילדם.<br />הדרכת צוותים חינוכיים לקיום שילוב יחידני מיטבי ואיכותי.<br />הנחיית בוגרים ומתבגרים בתחומי שינוי ומעבר (צבא, שירות לאומי, תעסוקה, דיור ומיומנויות בין אישיות).<br />בניית הרצאות מותאמות עבור גופים פרטים וציבוריים בנושא אוטיזם – שילוב והכלה.<br />הדרכת מעסיקים כיצד ניתן לשלב בתחומי עבודה שונים אדם על הרצף האוטיסטי.`,
   },
   {
-    title: "קצת עלי",
+    title: 'קצת עלי',
     content: `סיון ורונסקי, אמא לדניאל ומיכאל, שניהם נמצאים על הרצף האוטיסטי.<br />בחינוך מיוחד מאוניברסיטת בר אילן.<br />בחינוך מיוחד מהאוניברסיטה הפתוחה.<br />הכשרה בעקרונות יסוד בהתערבות חינוכית התנהגותית לילדים על רצף האוטיזם.<br />מומחית להוראה מתקנת ואסטרטגיות למידה.<br />פסיכותרפיסטית קוגנטיבית התנהגותית.<br />מאבחנת דידקטית מוסמכת.<br />חוקרת פוסט טראומה אצל הורים לילדים עם אוטיזם.<br />מתאמת שירות לבוגרים עם אוטיזם בתחומי שינוי ומעבר.<br />מנחה ומלווה משפחות לפני ואחרי האבחון, צעד צעד יחד.`,
   },
   {
-    title: "איפה מתקיימים המפגשים?",
+    title: 'איפה מתקיימים המפגשים?',
     content: `הקליניקה שלי ממוקמת בקריית אונו<br />*הרצאות יכולות להתקיים גם בבית הלקוח`,
   },
-];
+]
 
 export default function QnaSec() {
-  const [invis, setInvis] = useState(0);
-  const [isOpen, setisOpen] = useState(true);
+  const [invis, setInvis] = useState(0)
+  const [isOpen, setisOpen] = useState(true)
 
   function openQuestion(indexed) {
     if (invis === indexed) {
-      setisOpen(!isOpen);
-      setInvis(indexed);
+      setisOpen(!isOpen)
+      setInvis(indexed)
     } else {
-      setInvis(indexed);
+      setInvis(indexed)
     }
-    console.log(isOpen);
+    console.log(isOpen)
   }
   return (
-    <div className="FirstBox">
+    <div className='FirstBox'>
       <ul>
         {items.map((item, index) => (
-          <li key={index} className="listItemQnA">
-            <button onClick={() => openQuestion(index)} className="qnaBtn">
-              <img src={logo} alt="website logo" className="logoQnA" />
-              <strong className="qnATitle">{item.title}</strong>
+          <li key={index} className='listItemQnA'>
+            <button onClick={() => openQuestion(index)} className='qnaBtn'>
+              <img src={logo} alt='website logo' className='logoQnA' />
+              <strong className='qnATitle'>{item.title}</strong>
             </button>
             {!isOpen && index === invis && (
               <p
-                className="qnAP"
+                className='qnAP'
                 dangerouslySetInnerHTML={{ __html: item.content }}
               />
             )}
@@ -49,5 +49,5 @@ export default function QnaSec() {
         ))}
       </ul>
     </div>
-  );
+  )
 }
