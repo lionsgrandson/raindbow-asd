7a33aa01614b9ec70cea1cd9254fb95edcd09487
Moshe Schwartzberg <43815731+lionsgrandson@users.noreply.github.com>
2025-08-28 10:13:40 +0300

    edit: headshot image now rounded

--- src/component/contactSec.jsx
+++ src/component/contactSec.jsx
@@ -1,77 +1,81 @@
-import TextField from "@mui/material/TextField";
-import { MdEmail } from "react-icons/md";
-import { FaPhoneAlt } from "react-icons/fa";
-import "../App.css";
-import "./contactSec.css";
-import { FaLocationDot } from "react-icons/fa6";
-import sivanHeadShot from "../img/sivanHeadShot.png.png";
+import TextField from '@mui/material/TextField'
+import { MdEmail } from 'react-icons/md'
+import { FaPhoneAlt } from 'react-icons/fa'
+import '../App.css'
+import './contactSec.css'
+import { FaLocationDot } from 'react-icons/fa6'
+import sivanHeadShot from '../img/sivanHeadShot.png.png'
 function ContactSec() {
   return (
-    <section id="contact" className="FirstBox">
-      <h2 className="h1Con">יצירת קשר</h2>
-      <div className="contactContent">
-        <form method="POST" className="ConForm">
+    <section id='contact' className='FirstBox'>
+      <h2 className='h1Con'>יצירת קשר</h2>
+      <div className='contactContent'>
+        <form method='POST' className='ConForm'>
           <input
-            type="text"
-            placeholder="שם"
-            id="name"
-            autoComplete="name"
-            className="textField"
+            type='text'
+            placeholder='שם'
+            id='name'
+            autoComplete='name'
+            className='textField'
             required
           />
           <input
-            type="phone"
-            autoComplete="phone"
-            id="phone"
-            placeholder="טלפון"
-            className="textField"
+            type='phone'
+            autoComplete='phone'
+            id='phone'
+            placeholder='טלפון'
+            className='textField'
             required
           />
           <input
-            type="email"
-            autoComplete="email"
-            id="email"
-            placeholder="מייל"
-            className="textField"
+            type='email'
+            autoComplete='email'
+            id='email'
+            placeholder='מייל'
+            className='textField'
             required
           />
           <textarea
-            type="text"
-            placeholder="הודעה"
-            className="textArea"
+            type='text'
+            placeholder='הודעה'
+            className='textArea'
             required
           />
         </form>
-        <div className="midContactSec">
-          <img className="sivanHeadshotContact" src={sivanHeadShot} alt="" />
+        <div className='midContactSec'>
+          <img
+            className='sivanHeadshotContact'
+            src={sivanHeadShot}
+            alt='headshot of website owner'
+          />
           <button
-            type="submit"
+            type='submit'
             onClick={(e) => {
-              e.preventDefault();
-              console.log("btn clicked");
+              e.preventDefault()
+              console.log('btn clicked')
             }}
-            className="submitBTN"
+            className='submitBTN'
           >
             שלח
           </button>
         </div>
 
-        <section className="conDit">
+        <section className='conDit'>
           <div>
             <p c>קריית אונו והסביבה</p>
             <FaLocationDot />
           </div>
           <div>
-            <a href="tel:+972546495902">054-649-5902</a>
+            <a href='tel:+972546495902'>054-649-5902</a>
             <FaPhoneAlt />
           </div>
           <div>
-            <a href="mail:LEVANONSKI@GMAIL.COM">levanonski@gmail.com</a>
+            <a href='mail:LEVANONSKI@GMAIL.COM'>levanonski@gmail.com</a>
             <MdEmail />
           </div>
         </section>
       </div>
     </section>
-  );
+  )
 }
-export default ContactSec;
+export default ContactSec
