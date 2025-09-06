026a51e0ad75fc2b7a1a044d3d985aa80313f35c
Moshe Schwartzberg <43815731+lionsgrandson@users.noreply.github.com>
2025-09-06 12:57:00 +0300

    Mobile: footer section

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,25 +1,26 @@
-import { FaPhoneAlt } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
-import logoLongTxt from "../img/Logo_with_NO_shadow_text.png";
-import "./footer.css";
+import { FaPhoneAlt } from 'react-icons/fa'
+import { MdEmail } from 'react-icons/md'
+import logoLongTxt from '../img/Logo_with_NO_shadow_text.png'
+import './footer.css'
 export default function Footer() {
   return (
-    <div className="footerSec ">
+    <div className='footerSec '>
       <img
         src={logoLongTxt}
-        alt="on the left the slogan on the right a puzzle piece with rainbow colors"
+        alt='on the left the slogan on the right a puzzle piece with rainbow colors'
       />
-      <div className="ContactFooter">
+      <div className='ContactFooter'>
         <div>
-          <a href="tel:+972546495902">054-649-5902</a>
-          {"  "} <FaPhoneAlt />
+          <a href='tel:+972546495902'>054-649-5902</a>
+          {'  '} <FaPhoneAlt />
         </div>
         <div>
-          <a href="mailto:LEVANONSKI@GMAIL.COM">levanonski@gmail.com</a>
-          {"  "}
+          <a href='mailto:LEVANONSKI@GMAIL.COM'>levanonski@gmail.com</a>
+          {'  '}
           <MdEmail />
         </div>
       </div>
     </div>
-  );
+  )
 }
+// TODO pricacy policy etc and plugin
