eeafe20c14d1d67ff72cb56e45839ce2d74795c9
Moshe Schwartzberg <43815731+lionsgrandson@users.noreply.github.com>
2025-09-06 12:45:21 +0300

    Mobile: QnA section

--- src/component/qnaSec.jsx
+++ src/component/qnaSec.jsx
@@ -1,164 +1,143 @@
-import React, { useState } from "react";
-import "./qna.css";
-import logo from "../img/Logo_icon_noshadow_Custom.png";
+import React, { useState } from 'react'
+import './qna.css'
+import logo from '../img/Logo_icon_noshadow_Custom.png'
+
 const items = [
   {
-    title: "מהן התוכניות שאני מציעה? ",
-    content: "`ליווי אישי והדרכה של משלבות בחינוך הרגיל (שילוב יחידני).",
-    content2: "הדרכת הורים מותאמת להורים שבוחרים לשלב את ילדם.",
-    content3: "הדרכת צוותים חינוכיים לקיום שילוב יחידני מיטבי ואיכותי.",
+    title: 'מהן התוכניות שאני מציעה? ',
+    content: '`ליווי אישי והדרכה של משלבות בחינוך הרגיל (שילוב יחידני).',
+    content2: 'הדרכת הורים מותאמת להורים שבוחרים לשלב את ילדם.',
+    content3: 'הדרכת צוותים חינוכיים לקיום שילוב יחידני מיטבי ואיכותי.',
     content4:
-      "הנחיית בוגרים ומתבגרים בתחומי שינוי ומעבר (צבא, שירות לאומי, תעסוקה, דיור ומיומנויות בין אישיות).",
+      'הנחיית בוגרים ומתבגרים בתחומי שינוי ומעבר (צבא, שירות לאומי, תעסוקה, דיור ומיומנויות בין אישיות).',
     content5:
-      "בניית הרצאות מותאמות עבור גופים פרטים וציבוריים בנושא אוטיזם – שילוב והכלה.",
+      'בניית הרצאות מותאמות עבור גופים פרטים וציבוריים בנושא אוטיזם – שילוב והכלה.',
     content6:
-      "הדרכת מעסיקים כיצד ניתן לשלב בתחומי עבודה שונים אדם על הרצף האוטיסטי.`",
+      'הדרכת מעסיקים כיצד ניתן לשלב בתחומי עבודה שונים אדם על הרצף האוטיסטי.`',
   },
   {
-    title: "קצת עלי",
-    content: "סיון ורונסקי, אמא לדניאל ומיכאל, שניהם נמצאים על הרצף האוטיסטי.",
-    content2: "B.A בחינוך מיוחד מהאוניברסיטה הפתוחה.  ",
-    content3: " M.A בחינוך מיוחד מאוניברסיטת בר אילן.",
+    title: 'קצת עלי',
+    content: 'סיון ורונסקי, אמא לדניאל ומיכאל, שניהם נמצאים על הרצף האוטיסטי.',
+    content2: 'B.A בחינוך מיוחד מהאוניברסיטה הפתוחה.  ',
+    content3: ' M.A בחינוך מיוחד מאוניברסיטת בר אילן.',
     content4:
-      "ABA הכשרה בעקרונות יסוד בהתערבות חינוכית התנהגותית לילדים על רצף האוטיזם.",
-    content5: "מומחית להוראה מתקנת ואסטרטגיות למידה.",
-    content6: "CBT פסיכותרפיסטית קוגנטיבית התנהגותית.",
-    content7: "מאבחנת דידקטית מוסמכת.",
-    content8: "חוקרת פוסט טראומה אצל הורים לילדים עם אוטיזם.",
-    content9: "מתאמת שירות לבוגרים עם אוטיזם בתחומי שינוי ומעבר.",
-    content10: "מנחה ומלווה משפחות לפני ואחרי האבחון, צעד צעד יחד.",
+      'ABA הכשרה בעקרונות יסוד בהתערבות חינוכית התנהגותית לילדים על רצף האוטיזם.',
+    content5: 'מומחית להוראה מתקנת ואסטרטגיות למידה.',
+    content6: 'CBT פסיכותרפיסטית קוגנטיבית התנהגותית.',
+    content7: 'מאבחנת דידקטית מוסמכת.',
+    content8: 'חוקרת פוסט טראומה אצל הורים לילדים עם אוטיזם.',
+    content9: 'מתאמת שירות לבוגרים עם אוטיזם בתחומי שינוי ומעבר.',
+    content10: 'מנחה ומלווה משפחות לפני ואחרי האבחון, צעד צעד יחד.',
   },
   {
-    title: "איפה מתקיימים המפגשים?",
-    content: "הקליניקה שלי ממוקמת בקריית אונו",
-    content2: "*הרצאות יכולות להתקיים גם בבית הלקוח",
+    title: 'איפה מתקיימים המפגשים?',
+    content: 'הקליניקה שלי ממוקמת בקריית אונו',
+    content2: '*הרצאות יכולות להתקיים גם בבית הלקוח',
   },
-];
+]
 
 export default function QnaSec() {
-  const [invis, setInvis] = useState(0);
-  const [isOpen, setisOpen] = useState(true);
+  const [invis, setInvis] = useState(0)
+  const [isOpen, setisOpen] = useState(true)
 
   function openQuestion(indexed) {
     if (invis === indexed) {
-      setisOpen(!isOpen);
-      setInvis(indexed);
+      setisOpen(!isOpen)
+      setInvis(indexed)
     } else {
-      setInvis(indexed);
+      setInvis(indexed)
     }
-    console.log(isOpen);
+    console.log(isOpen)
   }
+
   return (
-    <div className="FirstBox">
+    <div className='FirstBox'>
       <ul>
         {items.map((item, index) => (
-          <li key={index} className="listItemQnA">
-            <button onClick={() => openQuestion(index)} className="qnaBtn">
+          <li key={index} className='listItemQnA'>
+            <button onClick={() => openQuestion(index)} className='qnaBtn'>
               {isOpen ? (
                 <svg
-                  width="2.5rem"
-                  height="2.5rem"
-                  className="arrowCloosedSvg"
-                  viewBox="-19.04 0 75.804 75.804"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="#000000"
-                  transform="rotate(270)"
-                  stroke="#000000"
-                  stroke-width="3.7902"
+                  width='2.5rem'
+                  height='2.5rem'
+                  className='arrowCloosedSvg'
+                  viewBox='-19.04 0 75.804 75.804'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='#000000'
+                  transform='rotate(270)'
+                  stroke='#000000'
+                  strokeWidth='3.7902'
                 >
-                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                  <g
-                    id="SVGRepo_tracerCarrier"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  ></g>
-                  <g id="SVGRepo_iconCarrier">
+                  <g id='SVGRepo_iconCarrier'>
                     <g
-                      id="Group_65"
-                      data-name="Group 65"
-                      transform="translate(-831.568 -384.448)"
+                      id='Group_65'
+                      data-name='Group 65'
+                      transform='translate(-831.568 -384.448)'
                     >
                       <path
-                        id="Path_57"
-                        data-name="Path 57"
-                        d="M833.068,460.252a1.5,1.5,0,0,1-1.061-2.561l33.557-33.56a2.53,2.53,0,0,0,0-3.564l-33.557-33.558a1.5,1.5,0,0,1,2.122-2.121l33.556,33.558a5.53,5.53,0,0,1,0,7.807l-33.557,33.56A1.5,1.5,0,0,1,833.068,460.252Z"
-                        fill="#0c2c67"
+                        id='Path_57'
+                        data-name='Path 57'
+                        d='M833.068,460.252a1.5,1.5,0,0,1-1.061-2.561l33.557-33.56a2.53,2.53,0,0,0,0-3.564l-33.557-33.558a1.5,1.5,0,0,1,2.122-2.121l33.556,33.558a5.53,5.53,0,0,1,0,7.807l-33.557,33.56A1.5,1.5,0,0,1,833.068,460.252Z'
+                        fill='#0c2c67'
                       ></path>
                     </g>
                   </g>
                 </svg>
               ) : (
                 <svg
-                  width="2.5rem"
-                  height="2.5rem"
-                  className="arrowCloosedSvg"
-                  viewBox="-19.04 0 75.804 75.804"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="#000000"
-                  transform="rotate(90)"
-                  stroke="#000000"
-                  stroke-width="3.7902"
+                  width='2.5rem'
+                  height='2.5rem'
+                  className='arrowCloosedSvg'
+                  viewBox='-19.04 0 75.804 75.804'
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='#000000'
+                  transform='rotate(90)'
+                  stroke='#000000'
+                  strokeWidth='3.7902'
                 >
-                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                  <g
-                    id="SVGRepo_tracerCarrier"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  ></g>
-                  <g id="SVGRepo_iconCarrier">
-                    {" "}
+                  <g id='SVGRepo_iconCarrier'>
                     <g
-                      id="Group_65"
-                      data-name="Group 65"
-                      transform="translate(-831.568 -384.448)"
+                      id='Group_65'
+                      data-name='Group 65'
+                      transform='translate(-831.568 -384.448)'
                     >
-                      {" "}
                       <path
-                        id="Path_57"
-                        data-name="Path 57"
-                        d="M833.068,460.252a1.5,1.5,0,0,1-1.061-2.561l33.557-33.56a2.53,2.53,0,0,0,0-3.564l-33.557-33.558a1.5,1.5,0,0,1,2.122-2.121l33.556,33.558a5.53,5.53,0,0,1,0,7.807l-33.557,33.56A1.5,1.5,0,0,1,833.068,460.252Z"
-                        fill="#0c2c67"
-                      ></path>{" "}
-                    </g>{" "}
+                        id='Path_57'
+                        data-name='Path 57'
+                        d='M833.068,460.252a1.5,1.5,0,0,1-1.061-2.561l33.557-33.56a2.53,2.53,0,0,0,0-3.564l-33.557-33.558a1.5,1.5,0,0,1,2.122-2.121l33.556,33.558a5.53,5.53,0,0,1,0,7.807l-33.557,33.56A1.5,1.5,0,0,1,833.068,460.252Z'
+                        fill='#0c2c67'
+                      ></path>
+                    </g>
                   </g>
                 </svg>
               )}
-              <strong className="qnATitle">{item.title}</strong>
+              <strong className='qnATitle'>{item.title}</strong>
             </button>
             {!isOpen && index === invis && (
-              <p
-                className="qnAP"
-                key={index}
-                // dangerouslySetInnerHTML={{ __html:  }}
-              >
-                <img src={logo} alt="website logo" className="logoQnA" />
-                {/* {item.content{index}} */}
-                {/* {(itemIndex = `content${index}`)} */}
-
+              <p className='qnAP' key={index}>
+                <img src={logo} alt='website logo' className='logoQnA' />
                 {item.content}
-
                 <br />
                 <br />
-
-                <img src={logo} alt="website logo" className="logoQnA" />
+                <img src={logo} alt='website logo' className='logoQnA' />
                 {item.content2}
                 {item.content3 ? (
                   <>
                     <br />
                     <br />
-                    <img src={logo} alt="website logo" className="logoQnA" />
+                    <img src={logo} alt='website logo' className='logoQnA' />
                     {item.content3}
                     <br />
                     <br />
-                    <img src={logo} alt="website logo" className="logoQnA" />
+                    <img src={logo} alt='website logo' className='logoQnA' />
                     {item.content4}
                     <br />
                     <br />
-                    <img src={logo} alt="website logo" className="logoQnA" />
+                    <img src={logo} alt='website logo' className='logoQnA' />
                     {item.content5}
                     <br />
                     <br />
-                    <img src={logo} alt="website logo" className="logoQnA" />
+                    <img src={logo} alt='website logo' className='logoQnA' />
                     {item.content6}
                     {item.content7 ? (
                       <>
@@ -166,32 +145,32 @@ export default function QnaSec() {
                         <br />
                         <img
                           src={logo}
-                          alt="website logo"
-                          className="logoQnA"
+                          alt='website logo'
+                          className='logoQnA'
                         />
                         {item.content7}
                         <br />
                         <br />
                         <img
                           src={logo}
-                          alt="website logo"
-                          className="logoQnA"
+                          alt='website logo'
+                          className='logoQnA'
                         />
                         {item.content8}
                         <br />
                         <br />
                         <img
                           src={logo}
-                          alt="website logo"
-                          className="logoQnA"
+                          alt='website logo'
+                          className='logoQnA'
                         />
                         {item.content9}
                         <br />
                         <br />
                         <img
                           src={logo}
-                          alt="website logo"
-                          className="logoQnA"
+                          alt='website logo'
+                          className='logoQnA'
                         />
                         {item.content10}
                         <br />
@@ -206,5 +185,5 @@ export default function QnaSec() {
         ))}
       </ul>
     </div>
-  );
+  )
 }
