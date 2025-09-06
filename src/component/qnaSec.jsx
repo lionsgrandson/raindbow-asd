38a0b4cbdd5734895172814d4ef1592d26a89f4a
Moshe Schwartzberg <43815731+lionsgrandson@users.noreply.github.com>
2025-09-06 13:21:13 +0300

    feat: minor fixes to the animations

--- src/component/qnaSec.jsx
+++ src/component/qnaSec.jsx
@@ -37,26 +37,24 @@ const items = [
 ]
 
 export default function QnaSec() {
-  const [invis, setInvis] = useState(0)
-  const [isOpen, setisOpen] = useState(true)
+  const [openIndex, setOpenIndex] = useState(null)
 
-  function openQuestion(indexed) {
-    if (invis === indexed) {
-      setisOpen(!isOpen)
-      setInvis(indexed)
-    } else {
-      setInvis(indexed)
-    }
-    console.log(isOpen)
+  const toggleQuestion = (index) => {
+    setOpenIndex(openIndex === index ? null : index)
   }
 
   return (
     <div className='FirstBox'>
       <ul>
-        {items.map((item, index) => (
-          <li key={index} className='listItemQnA'>
-            <button onClick={() => openQuestion(index)} className='qnaBtn'>
-              {isOpen ? (
+        {items.map((item, index) => {
+          const isOpen = openIndex === index
+          return (
+            <li key={index} className='listItemQnA'>
+              <button
+                onClick={() => toggleQuestion(index)}
+                className='qnaBtn'
+                aria-expanded={isOpen}
+              >
                 <svg
                   width='2.5rem'
                   height='2.5rem'
@@ -64,7 +62,7 @@ export default function QnaSec() {
                   viewBox='-19.04 0 75.804 75.804'
                   xmlns='http://www.w3.org/2000/svg'
                   fill='#000000'
-                  transform='rotate(270)'
+                  transform={isOpen ? 'rotate(90)' : 'rotate(270)'}
                   stroke='#000000'
                   strokeWidth='3.7902'
                 >
@@ -83,106 +81,31 @@ export default function QnaSec() {
                     </g>
                   </g>
                 </svg>
-              ) : (
-                <svg
-                  width='2.5rem'
-                  height='2.5rem'
-                  className='arrowCloosedSvg'
-                  viewBox='-19.04 0 75.804 75.804'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='#000000'
-                  transform='rotate(90)'
-                  stroke='#000000'
-                  strokeWidth='3.7902'
-                >
-                  <g id='SVGRepo_iconCarrier'>
-                    <g
-                      id='Group_65'
-                      data-name='Group 65'
-                      transform='translate(-831.568 -384.448)'
-                    >
-                      <path
-                        id='Path_57'
-                        data-name='Path 57'
-                        d='M833.068,460.252a1.5,1.5,0,0,1-1.061-2.561l33.557-33.56a2.53,2.53,0,0,0,0-3.564l-33.557-33.558a1.5,1.5,0,0,1,2.122-2.121l33.556,33.558a5.53,5.53,0,0,1,0,7.807l-33.557,33.56A1.5,1.5,0,0,1,833.068,460.252Z'
-                        fill='#0c2c67'
-                      ></path>
-                    </g>
-                  </g>
-                </svg>
+                <strong className='qnATitle'>{item.title}</strong>
+              </button>
+
+              {isOpen && (
+                <p className='qnAP'>
+                  {Object.keys(item).map(
+                    (key, i) =>
+                      key.startsWith('content') && (
+                        <span key={i}>
+                          <img
+                            src={logo}
+                            alt='website logo'
+                            className='logoQnA'
+                          />
+                          {item[key]}
+                          <br />
+                          <br />
+                        </span>
+                      )
+                  )}
+                </p>
               )}
-              <strong className='qnATitle'>{item.title}</strong>
-            </button>
-            {!isOpen && index === invis && (
-              <p className='qnAP' key={index}>
-                <img src={logo} alt='website logo' className='logoQnA' />
-                {item.content}
-                <br />
-                <br />
-                <img src={logo} alt='website logo' className='logoQnA' />
-                {item.content2}
-                {item.content3 ? (
-                  <>
-                    <br />
-                    <br />
-                    <img src={logo} alt='website logo' className='logoQnA' />
-                    {item.content3}
-                    <br />
-                    <br />
-                    <img src={logo} alt='website logo' className='logoQnA' />
-                    {item.content4}
-                    <br />
-                    <br />
-                    <img src={logo} alt='website logo' className='logoQnA' />
-                    {item.content5}
-                    <br />
-                    <br />
-                    <img src={logo} alt='website logo' className='logoQnA' />
-                    {item.content6}
-                    {item.content7 ? (
-                      <>
-                        <br />
-                        <br />
-                        <img
-                          src={logo}
-                          alt='website logo'
-                          className='logoQnA'
-                        />
-                        {item.content7}
-                        <br />
-                        <br />
-                        <img
-                          src={logo}
-                          alt='website logo'
-                          className='logoQnA'
-                        />
-                        {item.content8}
-                        <br />
-                        <br />
-                        <img
-                          src={logo}
-                          alt='website logo'
-                          className='logoQnA'
-                        />
-                        {item.content9}
-                        <br />
-                        <br />
-                        <img
-                          src={logo}
-                          alt='website logo'
-                          className='logoQnA'
-                        />
-                        {item.content10}
-                        <br />
-                        <br />
-                      </>
-                    ) : null}
-                  </>
-                ) : null}
-              </p>
-            )}
-          </li>
-        ))}
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
