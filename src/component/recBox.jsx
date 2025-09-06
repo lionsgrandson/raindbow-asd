3c8345417e9e1d4277d966daa40f7b6e42dc6ad9
Moshe Schwartzberg <43815731+lionsgrandson@users.noreply.github.com>
2025-09-06 12:47:50 +0300

    Mobile: recBox component

--- src/component/recBox.jsx
+++ src/component/recBox.jsx
@@ -1,40 +1,41 @@
-import "../App.css";
-import "./recBox.css";
-import logo from "../img/Logo_icon_noshadow.png";
+import '../App.css'
+import './recBox.css'
+import logo from '../img/Logo_icon_noshadow.png'
+
 function RecBox(props) {
   return (
-    <div className="FirstBox divrecbox">
-      <p className="StartRev">
+    <div className='FirstBox divrecbox'>
+      <p className='StartRev'>
         <img
           src={logo}
-          className="startLogo"
-          alt="puzzle peice with  rainbow colors"
+          className='startLogo'
+          alt='puzzle peice with rainbow colors'
         />
         <img
           src={logo}
-          className="startLogo"
-          alt="puzzle peice with  rainbow colors"
+          className='startLogo'
+          alt='puzzle peice with rainbow colors'
         />
         <img
           src={logo}
-          className="startLogo"
-          alt="puzzle peice with  rainbow colors"
+          className='startLogo'
+          alt='puzzle peice with rainbow colors'
         />
         <img
           src={logo}
-          className="startLogo"
-          alt="puzzle peice with  rainbow colors"
+          className='startLogo'
+          alt='puzzle peice with rainbow colors'
         />
         <img
           src={logo}
-          className="startLogo"
-          alt="puzzle peice with  rainbow colors"
+          className='startLogo'
+          alt='puzzle peice with rainbow colors'
         />
       </p>
-      <p className="RevPCon">{props.RevText}</p>
-      <p className="RevName">{props.RevName}</p>
+      <p className='RevPCon'>{props.RevText}</p>
+      <p className='RevName'>{props.RevName}</p>
     </div>
-  );
+  )
 }
 
-export default RecBox;
+export default RecBox
